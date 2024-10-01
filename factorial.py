ad28b816b83dce19297f28b1fb22cbfc0804d424
Meenu <63814693+meenub-18@users.noreply.github.com>
2024-10-01 16:40:47 +0530

    To calculate factorial

---
+++ factorial.py
@@ -0,0 +1,20 @@
+#factorial
+#5*4*3*2*1
+
+# while loop
+
+# n = 5
+# fact = 1
+# i = 1
+
+# while i<=5:
+#     fact = fact*i
+#     i = i+1
+# print(fact)
+
+# for i in range(1, n+1):   #normally range exclude the last no., but here n+1 include the last no. 5
+#     fact = fact*i
+# print(fact)
+
+
+
