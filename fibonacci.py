a2f532b0914938aa175bccf3aa959219a94bff89
Meenu <63814693+meenub-18@users.noreply.github.com>
2024-10-01 16:12:08 +0530

    Fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+no = int(input("Enter the number till you want the series:"))
+a = 0
+b = 1
+c = 0
+print(a)
+print(b)
+
+i = 1
+while i < no:
+    c = a + b
+    a = b
+    b = c
+    i = i + 1
+    print(c)
+
+print("The fibonacci series for the given no "+ str(no) + " is:",c)
